8fa501048e3a5aa393dc12b3d539469af0d392a8
17wook2 <alsdnr9999@g.skku.edu>
2023-11-14 17:52:59 +0900

    카카오톡 메시지 공유 파일 생성 - #15

--- src/components/tripplan/TripplanTest.vue
+++ src/components/tripplan/TripplanTest.vue
@@ -1,27 +1,58 @@
 <script setup>
 import { getTripPlanAndTripCoursesByPlanId } from "../../api/TripplanAPI";
-import { onMounted } from 'vue'
+import { onMounted, onBeforeMount } from 'vue'
 const { VITE_KAKAO_KEY } = import.meta.env;
-onMounted(() => {
-    Kakao.init()
-    Kakao.Share.createDefaultButton({
-    container: '#kakaotalk-sharing-btn',
-    objectType: 'text',
-    text:
-        '기본 템플릿으로 제공되는 텍스트 템플릿은 텍스트를 최대 200자까지 표시할 수 있습니다. 텍스트 템플릿은 텍스트 영역과 하나의 기본 버튼을 가집니다. 임의의 버튼을 설정할 수도 있습니다. 여러 장의 이미지, 프로필 정보 등 보다 확장된 형태의 카카오톡 공유는 다른 템플릿을 이용해 보낼 수 있습니다.',
-    link: {
-        mobileWebUrl: 'https://developers.kakao.com',
-        webUrl: 'https://developers.kakao.com',
-    },
-    });
+function sendLink() {
+    Kakao.Share.sendDefault({
+      objectType: 'feed',
+      content: {
+        title: '제목',
+        description: '설명',
+        imageUrl:
+          '이미지 도메인',
+        link: {
+          mobileWebUrl: '모바일 링크',
+          webUrl: '웹 링크',
+        },
+      },
+      social: {
+        likeCount: 286,  //좋아요 수
+        commentCount: 45,  //댓글 수
+        sharedCount: 845,  //공유 수
+      },
+      buttons: [
+        {
+          title: '웹으로 보기',  //첫 번째 버튼 
+          link: {
+            mobileWebUrl: '모바일 링크',  //버튼 클릭 시 이동 링크
+            webUrl: '웹 링크',
+          },
+        },
+        {
+          title: '앱으로 보기',  //두 번째 버튼
+          link: {
+            mobileWebUrl: '모바일 링크',
+            webUrl: '웹 링크',
+          },
+        },
+      ],
+    })
     
-})
+}
 
+onBeforeMount(() => {
+    Kakao.init(VITE_KAKAO_KEY);
+    console.log(Kakao.isInitialized());
+}),
+onMounted(() => {
+   
+})
 </script>
 
 <template>
     <div>
-        
+        <button @click="sendLink">공유 링크 보내기</button>
+        <button @click="sendLink">카카오 버튼</button>
     </div>
 </template>
 
