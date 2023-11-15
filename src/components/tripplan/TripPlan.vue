42b3203f8a4531cc92211ec804680ba8c62b4552
jangsumin <jsm0726@naver.com>
2023-11-15 12:02:55 +0900

    feat: html2canvas 라이브러리를 사용하여 이미지 다운로드 기능 추가 - #17

--- src/components/tripplan/TripPlan.vue
+++ src/components/tripplan/TripPlan.vue
@@ -1,6 +1,9 @@
 <script setup>
 import TripPlanElement from './TripPlanElement.vue'
+import html2canvas from 'html2canvas'
+import { ref } from 'vue'
 
+// 더미 데이터
 const tripPlans = {
   0: { id: 1, title: '복지리가 땡기는 지리산 여행', tags: ['복지리', '복어', '지리산', '등산'] },
   1: {
@@ -15,6 +18,26 @@ const tripPlans = {
   },
   3: { id: 4, title: '콜라 공장 방문', tags: ['제로', '콜라', '대세'] },
   4: { id: 5, title: '복지리가 땡기는 지리산 여행', tags: ['복지리', '복어', '지리산', '등산'] }
+}
+const captureTarget = ref()
+
+// 클립보드에 복사하는 방법
+// const captureImage = async () => {
+//   await html2canvas(captureTarget.value).then((canvas) => {
+//     canvas.toBlob((blob) => navigator.clipboard.write([new ClipboardItem({ 'image/png': blob })]))
+//   })
+// }
+
+// 이미지로 저장하는 방법
+const captureImage = async () => {
+  const canvas = await html2canvas(captureTarget.value, {
+    scale: 4
+  })
+  const image = canvas.toDataURL('image/jpeg')
+  const link = document.createElement('a')
+  link.href = image
+  link.download = 'trip_course.jpeg'
+  link.click()
 }
 </script>
 
@@ -32,7 +55,7 @@ const tripPlans = {
     </div>
 
     <div id="make-card">
-      <div id="card">
+      <div ref="captureTarget" id="card">
         <div class="main-logo"></div>
         <div class="stamp"></div>
 
@@ -79,12 +102,12 @@ const tripPlans = {
       </div>
 
       <div id="export">
-        <button>
-          <div class="download-button"></div>
+        <button @click="captureImage" id="download-button">
+          <div class="download-logo"></div>
           이미지로 다운로드
         </button>
         <button>
-          <div class="share-button"></div>
+          <div class="share-logo"></div>
           카카오톡 공유하기
         </button>
       </div>
@@ -155,7 +178,6 @@ const tripPlans = {
   gap: 15px;
   background-image: url('@/assets/images/purple-color-card.svg');
   background-position: center;
-  border-radius: 15px;
   box-shadow:
     0 5px 20px rgba(0, 0, 0, 0.19),
     0 3px 3px rgba(0, 0, 0, 0.21);
@@ -167,24 +189,21 @@ const tripPlans = {
 
 .main-logo {
   width: 200px;
-  height: 40px;
+  height: 45px;
   margin-left: -35px;
-  background-image: url('@/assets/images/logo.svg');
+  background-image: url('@/assets/images/small-logo.svg');
   background-position: center;
-  background-size: cover;
   background-repeat: no-repeat;
-  /* filter: blur(2px); */
 }
 
 .stamp {
   position: absolute;
-  top: 20px;
-  right: 20px;
-  width: 100px;
-  height: 100px;
+  top: 10px;
+  right: 10px;
+  width: 110px;
+  height: 110px;
   background-image: url('@/assets/images/stamp.svg');
   background-position: center;
-  background-size: cover;
   background-repeat: no-repeat;
 }
 
@@ -273,7 +292,7 @@ const tripPlans = {
   border-color: var(--sky-color);
 }
 
-.download-button {
+.download-logo {
   width: 25px;
   height: 25px;
   background-image: url('@/assets/images/download.svg');
@@ -281,7 +300,7 @@ const tripPlans = {
   background-size: cover;
   background-repeat: no-repeat;
 }
-.share-button {
+.share-logo {
   width: 25px;
   height: 25px;
   background-image: url('@/assets/images/share.svg');
