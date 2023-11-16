080453a31275bcb08fe142c217687d30666dcfb1
jangsumin <jsm0726@naver.com>
2023-11-17 01:47:39 +0900

    feat: 태그 기능 구현 및 적용 - #8

--- src/views/PostCreateView.vue
+++ src/views/PostCreateView.vue
@@ -4,17 +4,17 @@ import Editor from '@toast-ui/editor'
 import '@toast-ui/editor/dist/toastui-editor.css'
 import SitemapFooter from '../components/SitemapFooter.vue'
 
-import { onMounted, ref } from 'vue'
+import { onMounted, ref, watch } from 'vue'
 import { uploadImage, registArticle } from '@/api/BoardAPI.js'
-import { useMemberStore } from "@/stores/member";
-const memberStore = useMemberStore();
-const { userInfo } = memberStore;
+import { useMemberStore } from '@/stores/member'
+const memberStore = useMemberStore()
+const { userInfo } = memberStore
 
-const { VITE_VUE_API_URL } = import.meta.env;
-const router = useRouter();
-let editor;
+const { VITE_VUE_API_URL } = import.meta.env
+const router = useRouter()
+let editor
 onMounted(() => {
-  console.log(userInfo);
+  console.log(userInfo)
   editor = new Editor({
     el: document.querySelector('#editor'),
     width: '500px',
@@ -34,58 +34,80 @@ onMounted(() => {
     hooks: {
       async addImageBlobHook(blob, callback) {
         console.log(blob)
-        const formData = new FormData();
-        formData.append('image', blob);
-        uploadImage(
-          formData,
-          (response) => {
-            console.log(response);
-            const { saveFolder, originalFile, saveFile } = response.data;
-            console.log('서버에 저장된 파일명 : ', saveFile);
-            console.log('원본 파일명 : ', originalFile)
-            const imageUrl = `${VITE_VUE_API_URL}/board/image-print?savedFolder=${saveFolder}&filename=${saveFile}`;
-            callback(imageUrl, 'image alt attribute');
-          })
-        }
+        const formData = new FormData()
+        formData.append('image', blob)
+        uploadImage(formData, (response) => {
+          console.log(response)
+          const { saveFolder, originalFile, saveFile } = response.data
+          console.log('서버에 저장된 파일명 : ', saveFile)
+          console.log('원본 파일명 : ', originalFile)
+          const imageUrl = `${VITE_VUE_API_URL}/board/image-print?savedFolder=${saveFolder}&filename=${saveFile}`
+          callback(imageUrl, 'image alt attribute')
+        })
       }
+    }
   })
 })
 
 const categoryState = ref([false, false, false, false])
-const categoryList = ['여행지', '맛집', '코스', '기타'];
-const title = ref("");
-const categorySelected = ref("");
+const categoryList = ['여행지', '맛집', '코스', '기타']
+const title = ref('')
+const categorySelected = ref('')
 const onClickCategory = (num) => {
-  categorySelected.value = categoryList[num];
+  categorySelected.value = categoryList[num]
   categoryState.value.map((value, index) => {
     if (index === num) categoryState.value[index] = true
     else categoryState.value[index] = false
   })
 }
 function cancelPost() {
-  let confirm = confirm("작성중인 게시글이 있습니다. 떠나시겠습니까?")
-  if (confirm) router.push("/")
+  let confirm = confirm('작성중인 게시글이 있습니다. 떠나시겠습니까?')
+  if (confirm) router.push('/')
   return
 }
 function savePost() {
   const article = {
     subject: title.value,
-    userId : 'ssafy',
-    category : categorySelected.value,
-    content : editor.getHTML(),
+    userId: 'ssafy',
+    category: categorySelected.value,
+    content: editor.getHTML()
   }
   if (editor.getMarkdown().length < 1) {
-    alert('에디터 내용을 입력해 주세요.');
-    throw new Error('editor content is required!');
+    alert('에디터 내용을 입력해 주세요.')
+    throw new Error('editor content is required!')
   }
-  registArticle(article, (response) => {
-    console.log(response)
-  }, (error) => {
-    console.log(error)
-  })
-  
+  registArticle(
+    article,
+    (response) => {
+      console.log(response)
+    },
+    (error) => {
+      console.log(error)
+    }
+  )
 }
 
+const tags = ref([])
+const tag = ref('')
+const isInputAvailable = ref(true)
+
+const onChangeTagInputValue = (event) => {
+  tag.value = event.target.value
+  // console.log(tag.value)
+}
+
+const addTag = (event) => {
+  if (tag.value.length > 0 && tags.value.length < 5) {
+    tags.value.push(tag.value.trim())
+    tag.value = ''
+  }
+  if (tags.value.length === 5) isInputAvailable.value = false
+}
+
+const removeTag = (index) => {
+  tags.value.splice(index, 1)
+  if (tags.value.length < 5) isInputAvailable.value = true
+}
 </script>
 
 <template>
@@ -120,12 +142,26 @@ function savePost() {
 
     <div class="title-input">
       <p>제목</p>
-      <input type="text" placeholder="제목을 적어주세요." v-model="title"/>
+      <input type="text" placeholder="제목을 적어주세요." v-model="title" />
     </div>
 
-    <div class="tag-input">
+    <div class="tag-wrap">
       <p>태그</p>
-      <input type="text" placeholder="태그를 추가해보세요!" />
+      <div class="tag-input">
+        <div v-for="(tag, index) in tags" :key="index" class="tagging">
+          {{ tag }}
+          <span @click="removeTag(index)">x</span>
+        </div>
+        <input
+          v-show="isInputAvailable"
+          :value="tag"
+          @input="onChangeTagInputValue"
+          @keypress.enter.prevent="addTag"
+          type="text"
+          placeholder="태그를 추가해보세요!"
+          class="tag-text"
+        />
+      </div>
       <span>최대 5개의 태그를 달 수 있어요!</span>
     </div>
 
@@ -145,6 +181,41 @@ function savePost() {
 </template>
 
 <style scoped>
+.tag-input {
+  box-sizing: border-box;
+  width: 800px;
+  height: 60px;
+  padding: 15px;
+  display: flex;
+  gap: 10px;
+  font-size: 16px;
+  font-weight: 600;
+  border: 1px solid var(--tag-color);
+  border-radius: 4px;
+  outline-color: var(--sky-color);
+}
+
+.tag-input span {
+  cursor: pointer;
+  opacity: 0.75;
+}
+
+.tagging {
+  box-sizing: border-box;
+  display: flex;
+  align-items: center;
+  gap: 5px;
+  padding: 5px;
+  background-color: var(--tag-color);
+  border-radius: 5px;
+}
+
+.tag-text {
+  font-size: 16px;
+  border: none;
+  outline: none;
+}
+
 #wrap {
   position: relative;
   width: 100%;
@@ -165,7 +236,7 @@ function savePost() {
 
 .category-input,
 .title-input,
-.tag-input,
+.tag-wrap,
 .editor-input {
   margin: 15px 0;
   display: flex;
@@ -175,7 +246,7 @@ function savePost() {
 
 .category-input p,
 .title-input p,
-.tag-input p,
+.tag-wrap p,
 .editor-input p {
   font-size: 16px;
   font-weight: 500;
@@ -204,24 +275,25 @@ function savePost() {
 }
 
 .title-input input {
+  width: 800px;
+  height: 60px;
+  padding: 20px;
+  font-size: 16px;
+  border: 1px solid var(--tag-color);
+  border-radius: 4px;
+  outline: none;
+}
+
+/* .tag-wrap input {
   width: 600px;
   padding: 20px;
   font-size: 16px;
   border: 1px solid var(--tag-color);
   border-radius: 4px;
   outline-color: var(--sky-color);
-}
+} */
 
-.tag-input input {
-  width: 600px;
-  padding: 20px;
-  font-size: 16px;
-  border: 1px solid var(--tag-color);
-  border-radius: 4px;
-  outline-color: var(--sky-color);
-}
-
-.tag-input span {
+.tag-wrap span {
   font-size: 14px;
   color: var(--sub-font-color);
 }
